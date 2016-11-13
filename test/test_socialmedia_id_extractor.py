2d15e49d40f0536bec1eee3239a156b21e1a4483
Rahul Kapoor <rahulkapoor.bits@gmail.com>
2016-11-13 13:39:27 -0800

    Correcting Test

--- test/test_socialmedia_id_extractor.py
+++ test/test_socialmedia_id_extractor.py
@@ -29,8 +29,7 @@ class TestSocialMediaExtractor(unittest.TestCase):
 
         print updated_doc
 
-        #self.assertEquals(updated_doc['ad_type'][0]['value'], 'massage-parlor')
-        self.assertEquals(True, True)
+        self.assertEquals(updated_doc['social_media_ids'][0]['value'], {'twitter': 'diamondsquirt', 'instagram': None})
 
 if __name__ == '__main__':
     unittest.main()
