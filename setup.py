bffbbc514150a465667eabb5be1403fcd2d48a7a
jasonslepicka <jasonslepicka@gmail.com>
2016-11-13 17:41:43 -0800

    Update version

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ config = {
     'url': 'https://github.com/usc-isi-i2/dig-socialmedia-id-extractor',
     'download_url': 'https://github.com/usc-isi-i2/dig-socialmedia-id-extractor',
     'author_email': 'rahulkap@isi.edu',
-    'version': '0.1.1',
-    'install_requires': ['digExtractor>=0.2.0'],
+    'version': '0.3.0',
+    'install_requires': ['digExtractor>=0.3.0'],
     # these are the subdirs of the current directory that we care about
     'packages': ['digSocialMediaIdExtractor'],
     'scripts': [],
