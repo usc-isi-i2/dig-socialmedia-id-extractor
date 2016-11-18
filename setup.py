e6861be6cbe3cfc11a7af6289ff398d363657577
Amandeep Singh <amandeep.s.saggu@gmail.com>
2016-11-17 18:00:33 -0800

    make the unit test run

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ config = {
     'url': 'https://github.com/usc-isi-i2/dig-socialmedia-id-extractor',
     'download_url': 'https://github.com/usc-isi-i2/dig-socialmedia-id-extractor',
     'author_email': 'rahulkap@isi.edu',
-    'version': '0.3.2',
-    'install_requires': ['digExtractor>=0.3.0'],
+    'version': '0.3.3',
+    'install_requires': ['digExtractor>=0.3.0', 'digDictionaryExtractor>=0.3.0'],
     # these are the subdirs of the current directory that we care about
     'packages': ['digSocialMediaIdExtractor'],
     'scripts': [],
--- test/test_socialmedia_id_extractor.py
+++ test/test_socialmedia_id_extractor.py
@@ -1,22 +1,24 @@
 import codecs
 import unittest
 import json
+import os
 
 from digExtractor.extractor_processor import ExtractorProcessor
 from digSocialMediaIdExtractor import socialmedia_id_extractor
 
+en_words = json.load(codecs.open(os.path.join(os.path.dirname(__file__), "english.json"), 'r', 'utf-8'))
+fr_words = json.load(codecs.open(os.path.join(os.path.dirname(__file__), "french.json"), 'r', 'utf-8'))
+
 
 class TestSocialMediaExtractor(unittest.TestCase):
-    def __init__(self):
-        self.en_words = json.load(codecs.open("english.json", 'r', 'utf-8'))
-        self.fr_words = json.load(codecs.open("french.json", 'r', 'utf-8'))
+
 
     def test_socialmedia_id_extractor(self):
         doc = {"tokens":["adair","location",":","escorts","missouri","escorts","kansas",
         "city","escorts","adair","my","information","follow","me","on","twitter","@","DiamondSquirt","location",":","kansas","city","escorts","type",":"]}
 
         #print doc["tokens"][0]["result"][0]["value"]
-        extractor = socialmedia_id_extractor.SocialMediaIdExtractor(self.en_words, self.fr_words)
+        extractor = socialmedia_id_extractor.SocialMediaIdExtractor(en_words, fr_words)
         extractor_processor = ExtractorProcessor().set_input_fields('tokens').set_output_field('social_media_ids').set_extractor(extractor)
 
         updated_doc = extractor_processor.extract(doc)
@@ -25,7 +27,7 @@ class TestSocialMediaExtractor(unittest.TestCase):
 
     def test_missing_tokens(self):
         doc = {"tokens":[]}
-        extractor = socialmedia_id_extractor.SocialMediaIdExtractor(self.en_words, self.fr_words)
+        extractor = socialmedia_id_extractor.SocialMediaIdExtractor(en_words, fr_words)
         extractor_processor = ExtractorProcessor().set_input_fields('tokens').set_output_field('social_media_ids').set_extractor(extractor)
 
         updated_doc = extractor_processor.extract(doc)
@@ -36,7 +38,7 @@ class TestSocialMediaExtractor(unittest.TestCase):
 
     def test_empty_tokens(self):
         doc = {}
-        extractor = socialmedia_id_extractor.SocialMediaIdExtractor(self.en_words, self.fr_words)
+        extractor = socialmedia_id_extractor.SocialMediaIdExtractor(en_words, fr_words)
         extractor_processor = ExtractorProcessor().set_input_fields('tokens').set_output_field('social_media_ids').set_extractor(extractor)
 
         updated_doc = extractor_processor.extract(doc)
